c7eff5bd9d8849c2198ffbfc94f9f0d76d9a848f
Andrew Sutherland <azsuth@gmail.com>
2020-06-08 22:00:58 -0500

    instructions copy change

--- web/src/components/home/instructions.js
+++ web/src/components/home/instructions.js
@@ -11,10 +11,9 @@ const Instructions = () => (
     you secretly Kenny G's biggest fan? Is coffee not your cup of tea? Let's
     find out!
     <h2>Overview</h2>
-    The game is broken into multiple rounds. In each round, a player is chosen
-    at random to be the "Ranker." If you're the Ranker, you'll receive four
-    trivial yet polarizing topics to rank from best to worst based on your
-    personal preference.
+    Throughout the game each player will take turns being the "Ranker." When
+    you're the Ranker, you'll receive 4 trivial yet polarizing topics to rank
+    from best to worst based on your personal preference.
     <br />
     <br />
     For example, you may see: Road Trips, Rainy Sundays, Dave Matthews Band, and
@@ -22,23 +21,20 @@ const Instructions = () => (
     your response. Be sure to keep your response a secret from the other
     players!
     <br />
-    <br />
     All other players in the game will try to guess how you responded, then
-    "lock in" on their own screens.
+    "lock in" on their own screens. Once everyone is locked in, you reveal your
+    response.
     <br />
     <br />
-    Once everyone is locked in, you reveal your responses! Have fun with it!
-    <br />
-    <br />
-    After revealing all four topics, a new round begins and a new Ranker is
-    assigned. The game continues until there are no more topics left to rank.
+    Once you have revealed the position of all 4 topics, another player will be
+    prompted to take their turn as the Ranker. The game continues until there
+    are no more topics left to rank.
     <h2>How to start</h2>
-    Once you have a group of players together, click START A NEW GAME and follow
-    the prompts. You'll receive a game code to share with other players. The
-    rest of the players click JOIN AN EXISTING GAME, enter the code and start
-    playing!
+    Once you have a group of players together, click "Start a Game" and follow
+    the prompts. You'll receive a game code to share with the other players. The
+    rest of the players click "Join a Game" enter the code, and start playing.
     <h2>How to win</h2>
-    Players get one point for each topic that they place in the exact same
+    Players get 1 point for each topic that they place in the exact same
     position as the Ranker. The player with the most points at the end of the
     game wins!
   </div>
