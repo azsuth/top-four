d35c2f68df7963f1cda264f69bf637225ce39c2b
Andrew Sutherland <azsuth@gmail.com>
2020-11-10 18:53:55 -0600

    open book superlative

--- web/src/utilities/superlatives.js
+++ web/src/utilities/superlatives.js
@@ -1,7 +1,76 @@
 import { toAllPlayersWithScores } from 'utilities/state_mapping';
 
 function generateSuperlatives(gameData) {
-  return [theWinner(gameData), theLoser(gameData)];
+  const topicGuesses = getTopicGuesses(gameData);
+  const rankers = getRankers(topicGuesses);
+
+  return [
+    theWinner(gameData),
+    theLoser(gameData),
+    theOpenBook(gameData, { topicGuesses, rankers })
+  ].filter(superlative => !!superlative);
+}
+
+function getTopicGuesses({ guesses, topics }) {
+  return Object.keys(guesses).reduce((accum, playerUid) => {
+    Object.keys(guesses[playerUid]).forEach(topicUid => {
+      if (!accum[topicUid]) {
+        accum[topicUid] = {
+          correctRank: topics[topicUid].rank,
+          guesses: [],
+          ranker: undefined,
+          numCorrect: 0,
+          numIncorrect: 0
+        };
+      }
+
+      if (guesses[playerUid][topicUid] === 'active') {
+        accum[topicUid].ranker = playerUid;
+      } else {
+        const guess = guesses[playerUid][topicUid];
+
+        accum[topicUid].guesses.push(guess);
+
+        if (guess === accum[topicUid].correctRank) {
+          accum[topicUid].numCorrect += 1;
+        } else {
+          accum[topicUid].numIncorrect += 1;
+        }
+      }
+    });
+
+    return accum;
+  }, {});
+}
+
+function getRankers(topicGuesses) {
+  return Object.keys(topicGuesses).reduce((accum, topicUid) => {
+    const topicGuess = topicGuesses[topicUid];
+    const rankerUid = topicGuess.ranker;
+
+    if (!accum[rankerUid]) {
+      accum[rankerUid] = {
+        numberCorrect: 0,
+        numberLied: 0,
+        numberPerfect: 0
+      };
+    }
+
+    accum[rankerUid].numberCorrect += topicGuess.numberCorrect;
+
+    if (
+      topicGuess.numberCorrect === 0 &&
+      new Set(topicGuess.guesses).size === 1
+    ) {
+      accum[rankerUid].numberLied += 1;
+    }
+
+    if (topicGuess.numIncorrect === 0) {
+      accum[rankerUid].numberPerfect += 1;
+    }
+
+    return accum;
+  }, {});
 }
 
 function theWinner({ guesses, players, topics }) {
@@ -53,6 +122,8 @@ function theLoser({ guesses, players, topics }) {
     ({ score }) => score === losingScore
   );
 
+  if (losingPlayers.length === playerScores.length) return null;
+
   if (losingPlayers.length === 1) {
     const losingPlayer = losingPlayers[0];
     const totalGuesses = Object.values(guesses[losingPlayer.uid]).filter(
@@ -77,5 +148,44 @@ function theLoser({ guesses, players, topics }) {
   };
 }
 
-export { theWinner, theLoser };
+function theOpenBook({ players }, { rankers }) {
+  const sortedRankers = Object.keys(rankers)
+    .map(rankerUid => ({
+      uid: rankerUid,
+      ...rankers[rankerUid]
+    }))
+    .sort(
+      ({ numberPerfect: numberPerfectA }, { numberPerfect: numberPerfectB }) =>
+        numberPerfectB - numberPerfectA
+    );
+
+  const mostPerfectCount = sortedRankers[0].numberPerfect;
+  const mostPerfectPlayers = sortedRankers.filter(
+    ({ numberPerfect }) => numberPerfect === mostPerfectCount
+  );
+
+  if (mostPerfectPlayers.length === Object.keys(players).length) return null;
+
+  if (mostPerfectPlayers.length === 1) {
+    const mostPerfectPlayer = players[mostPerfectPlayers[0].uid];
+
+    return {
+      header: 'The Open Book',
+      subheader: 'No need to judge you by your cover',
+      recipient: mostPerfectPlayer.name,
+      footer: `The group guessed your rankings 100% correct ${mostPerfectCount} times!`
+    };
+  }
+
+  return {
+    header: 'The Open Book',
+    subheader: 'A few of you need some more mystery in your life',
+    recipient: mostPerfectPlayers
+      .map(({ uid }) => players[uid].name)
+      .join(', '),
+    footer: `The group guessed your rankings 100% correct ${mostPerfectCount} times!`
+  };
+}
+
+export { theWinner, theLoser, theOpenBook, getTopicGuesses };
 export default generateSuperlatives;
--- web/tests/utilities/superlatives.test.js
+++ web/tests/utilities/superlatives.test.js
@@ -4,7 +4,12 @@ jest.mock('utilities/state_mapping', () => ({
   toAllPlayersWithScores: jest.fn()
 }));
 
-import { theWinner, theLoser } from 'utilities/superlatives';
+import {
+  theWinner,
+  theLoser,
+  theOpenBook,
+  getTopicGuesses
+} from 'utilities/superlatives';
 
 describe('superlative functions', () => {
   afterEach(() => {
@@ -90,6 +95,146 @@ describe('superlative functions', () => {
       ]);
 
       expect(theLoser({}).recipient).toBe('Andrew, Harrison');
+    });
+
+    it('returns null if all players are losers', () => {
+      toAllPlayersWithScores.mockReturnValue([
+        { uid: '12345', score: 10, active: true, name: 'Emily' },
+        { uid: '23456', score: 10, active: true, name: 'Andrew' },
+        { uid: '34567', score: 10, active: true, name: 'Harrison' }
+      ]);
+
+      expect(theLoser({})).toBe(null);
+    });
+  });
+
+  describe('theOpenBook', () => {
+    it('calculates the player whose rankings were guessed 100% the most', () => {
+      const rankers = {
+        12345: { numberPerfect: 3 },
+        23456: { numberPerfect: 2 },
+        34567: { numberPerfect: 0 }
+      };
+      const players = {
+        12345: { name: 'Andrew' },
+        23456: { name: 'Emily' },
+        34567: { name: 'Harrison' }
+      };
+
+      const superlative = theOpenBook({ players }, { rankers });
+
+      expect(superlative.recipient).toBe('Andrew');
+      expect(superlative.footer).toBe(
+        'The group guessed your rankings 100% correct 3 times!'
+      );
+    });
+
+    it('calculates a tie', () => {
+      const rankers = {
+        12345: { numberPerfect: 3 },
+        23456: { numberPerfect: 3 },
+        34567: { numberPerfect: 0 }
+      };
+      const players = {
+        12345: { name: 'Andrew' },
+        23456: { name: 'Emily' },
+        34567: { name: 'Harrison' }
+      };
+
+      const superlative = theOpenBook({ players }, { rankers });
+
+      expect(superlative.recipient).toBe('Andrew, Emily');
+      expect(superlative.footer).toBe(
+        'The group guessed your rankings 100% correct 3 times!'
+      );
+    });
+
+    it('returns null if all players tied', () => {
+      const rankers = {
+        12345: { numberPerfect: 0 },
+        23456: { numberPerfect: 0 },
+        34567: { numberPerfect: 0 }
+      };
+      const players = {
+        12345: { name: 'Andrew' },
+        23456: { name: 'Emily' },
+        34567: { name: 'Harrison' }
+      };
+
+      expect(theOpenBook({ players }, { rankers })).toBe(null);
+    });
+  });
+
+  describe('getTopicGuesses', () => {
+    const guesses = {
+      12345: {
+        a: 0,
+        b: 1,
+        c: 2,
+        d: 3,
+        e: 'active',
+        f: 'active',
+        g: 'active',
+        h: 'active',
+        i: 3,
+        j: 2,
+        k: 1,
+        l: 0
+      },
+      23456: {
+        a: 'active',
+        b: 'active',
+        c: 'active',
+        d: 'active',
+        e: 0,
+        f: 1,
+        g: 2,
+        h: 3,
+        i: 3,
+        j: 2,
+        k: 1,
+        l: 0
+      },
+      34567: {
+        a: 3,
+        b: 2,
+        c: 1,
+        d: 0,
+        e: 0,
+        f: 1,
+        g: 2,
+        h: 3,
+        i: 'active',
+        j: 'active',
+        k: 'active',
+        l: 'active'
+      }
+    };
+    const topics = {
+      a: { rank: 0 },
+      b: { rank: 1 },
+      c: { rank: 2 },
+      d: { rank: 3 },
+      e: { rank: 0 },
+      f: { rank: 1 },
+      g: { rank: 2 },
+      h: { rank: 3 },
+      i: { rank: 0 },
+      j: { rank: 1 },
+      k: { rank: 2 },
+      l: { rank: 3 }
+    };
+
+    it('returns an object of topic IDs with their guesses', () => {
+      const topicGuesses = getTopicGuesses({ guesses, topics });
+
+      expect(topicGuesses.a).toEqual({
+        correctRank: 0,
+        guesses: [0, 3],
+        numCorrect: 1,
+        numIncorrect: 1,
+        ranker: '23456'
+      });
     });
   });
 });
