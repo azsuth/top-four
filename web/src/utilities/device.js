37c20226816dc7166bedb49a44a148946c1f80f6
Andrew Sutherland <azsuth@gmail.com>
2021-02-20 14:07:55 -0600

    fix window undefined

--- web/src/utilities/device.js
+++ web/src/utilities/device.js
@@ -1,17 +1,17 @@
 export function isPhone() {
-  return window.matchMedia
+  return window && window.matchMedia
     ? window.matchMedia('(max-width: 767px)').matches
     : false;
 }
 
 export function isTablet() {
-  return window.matchMedia
+  return window && window.matchMedia
     ? window.matchMedia('(min-width: 768px) and (max-width: 1023px)').matches
     : false;
 }
 
 export function isDesktop() {
-  return window.matchMedia
+  return window && window.matchMedia
     ? window.matchMedia('(min-width: 1024px)').matches
     : null;
 }
